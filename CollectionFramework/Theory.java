b38046ba0e339f46a1613156de362df662c9e3dd
poojangit <poojang147@gmail.com>
2025-01-10 14:53:41 +0530

    [POOJA] Added : Collection - collection defination, difference between arrays and collection , vaious methods in collections, collection hierarchy

---
+++ CollectionFramework/Theory.java
@@ -0,0 +1,45 @@
+//todo ----------------------COLLECTION FRAMEWORK---------------------
+
+//^ Collections
+//! Collections are the pre-defined interface present in java.util package.
+//! It was intoduced from JDK 1.2
+//! Collection is used to store the group of objects.
+
+//^ Difference between arrays and collections
+
+//& Arrays
+//! Homogeneous in nature
+//! Fixed size
+//! Does not have any underlined data structure
+//! no predefined methods
+//! Cannot perform the curd operation in arrays
+
+//& Collections
+//! Both homogeneous and heterogeneous in nature
+//! Dynamic size
+//! Every collection will have a predefined data structure.
+//! many predefined methods
+//! Curd operations can be performed in collections
+
+//^ Important methods present in collection Interface
+
+//& add()
+//! Adds the object into the collection
+//& addAll()
+//! used to add one collection into the another collection
+//& contains()
+//! checks if the object is present in the collection or not
+//& containsAll()
+//! checks if the collection is present in another collection or not
+//& remove()
+//! removes the object from the collection based on the index position
+//& removeAll()
+//! removes the one collection from another collection
+//& size()
+//! used to find the number of object present in the collection
+//& clear()
+//! used to remove or delete all the collection object from the collection
+//& isEmpty()
+//! used to check if the collection is empty or not
+//& iterator()
+//! used to traverse the object from a collection
