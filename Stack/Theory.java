7f6df704f1f1cc9118f62b34f4d621c0ee14f8d2
poojangit <poojang147@gmail.com>
2025-01-13 15:46:20 +0530

    [POOJA] Stack : Theory part in stack

---
+++ Stack/Theory.java
@@ -0,0 +1,7 @@
+//todo ----------------Stack--------------------
+
+//^ Stack is a pre-defined class present in java.util package
+//^ stack is called a legacy collection because it was introduced in jdk 1.0;
+//^ Operation of stack is LIFO)Last in first out)
+//^ The insertion operation in stack is push 
+//^ The deletion operation in stack is pop
