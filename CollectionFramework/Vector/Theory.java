3311333031c2fee89ac72f1cd39384b1cce80dc7
poojangit <poojang147@gmail.com>
2025-01-13 14:52:41 +0530

    [POOJA] Vector: Theory part on vectoe

---
+++ CollectionFramework/Vector/Theory.java
@@ -0,0 +1,21 @@
+//todo ----------Vector-------------------
+
+//^ Vector is a predefined class preset n jvaa.util package
+//^ Vector ia a legacy collection because it was introduced from jdk 1.0.
+//^ Initial capacity of the vector is 10
+//^ Incremental capacity of the vector is cc*2
+//^ The underlining data structure of vector is resizable array or growable array
+//^ Vector is a thread safe
+
+// ^ The vector implements the following interfaces
+//! List
+//! RandomAccess
+//! Clonable
+//! Serializable
+
+//^ contructors present in the vector class
+//! vector()
+//! vector(int initialCapacity)
+//! vector(int initialCapacity, int incrementalCapacity)
+//! vector(Collection c)
+
