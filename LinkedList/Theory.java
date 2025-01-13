94d328b1a61c49dfd5586d131fc2d562e78a2be7
poojangit <poojang147@gmail.com>
2025-01-13 13:05:37 +0530

    [POOJA] Theory part on linked list

---
+++ LinkedList/Theory.java
@@ -0,0 +1,19 @@
+//todo -----------LINKED LIST------------------
+
+//^ Linked list is a predefined class present in java.util package.
+//^ It is introduced from JDK 1.2
+//^ The objects inside the linked list stored in the form of nodes.
+//^ Where each node will store the object, address of the next node and the previous node.
+//^ Linked list does not have the initial capacity and the incremental capacity
+//^ The underlining data structure of linked list is doubly linked list.
+//^ Memory allocation is non sequential.
+
+//^ The linked list implements the following interfaces
+//! clonable
+//! serializable
+//! dequeue
+//! list
+
+//^ constructors present in the linked list class -
+//! LinkedList()
+//! LinkedList(Collection c)
