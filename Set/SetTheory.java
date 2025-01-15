606c6630e310263cd19abcbbc1b6ab06c11efe53
poojangit <poojang147@gmail.com>
2025-01-15 11:07:15 +0530

    [POOJA] Theory part on set

---
+++ Set/SetTheory.java
@@ -0,0 +1,18 @@
+//todo ---------SETS---------------
+
+//^ set is a predefined interface present in java.util pacakage.
+//^ the set was introduced from jdk 1.2
+//^ we cannot use get() or set() because that will return a value based on the index and set does not have index based
+//^ we can iterate through for-each or iterator.
+
+//^ The implementation class of set interface -
+//! HashSet
+//! LinkedHashSet
+//! TreeSet
+
+//^Specifications of set interface
+//! Insertion order is not maintained
+//! Dublication is not allowed
+//! It is not index based
+//! null values can be stored
+
