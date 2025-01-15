07776bff3c1fd0ef611094a77e896f432770ed74
poojangit <poojang147@gmail.com>
2025-01-15 11:26:09 +0530

    [POOJA] HashSet : Theory part on hash set

---
+++ HashSet/TheoryHashSet.java
@@ -0,0 +1,34 @@
+//todo ------------HASH SET------------------
+
+//^ Hash set is a pre-defined class present in java.util package.
+//^ Hash set was introduced from JDK 1.2
+//^ The initial capacity of hash set is 16.
+//^ the load factor/ fill ratio of hash set is 0.75 or 75%
+//^ The underlining data structure of hash set is Hash Table.
+
+//^ Hash set implements 
+//! set
+//! clonable
+//! serializable
+
+//^ the constructors present in hash table are
+//! HashSet()
+//! HashSet(int initialCapacity)
+//! HashSet(int initialCapacity, float loadFactor)
+//! HashSet(Constructor c)
+
+//^ Key features
+//! Hash set does not allow dublicate elements
+//! It does not maintain the order of execution
+//! Allows one null value
+//! It offers constant time performance for add, remove and contains
+//! Thread safe : not synchronized
+
+//^ Methods used in Hashset
+//! add()
+//! remove()
+//! clear()
+//! isEmpty()
+//! contains()
+//! size()
+//! interator()
